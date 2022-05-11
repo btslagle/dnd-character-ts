a085d01925f7520d767a18abd388de4b3295d2ca
Brad Slagle <btslagle@gmail.com>
2022-05-11 11:28:28 -0500

    Get test two to pass

--- dnd-character.ts
+++ dnd-character.ts
@@ -1,1 +1,57 @@
-export class DnDCharacter {}
+export class DnDCharacter {
+  strength: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+  wisdom: number;
+  charisma: number;
+  //  hitpoints: number;
+
+  constructor() {
+    this.strength = DnDCharacter.generateAbilityScore();
+    this.dexterity = DnDCharacter.generateAbilityScore();
+    this.constitution = DnDCharacter.generateAbilityScore();
+    this.intelligence = DnDCharacter.generateAbilityScore();
+    this.wisdom = DnDCharacter.generateAbilityScore();
+    this.charisma = DnDCharacter.generateAbilityScore();
+  }
+
+  static randomDiceRoll() {
+    return Math.ceil(Math.random() * 6);
+  }
+  static topThreeDiceRolls() {
+    const diceRoll = [];
+    while (diceRoll.length < 4) {
+      diceRoll.push(this.randomDiceRoll());
+    }
+    const lowestDice = Math.min(...diceRoll);
+    return diceRoll.filter((roll) => roll != lowestDice);
+  }
+  static generateAbilityScore() {
+    const topThree = this.topThreeDiceRolls();
+    const sum = 0;
+    return topThree.reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      sum
+    );
+  }
+}
+/*  get rollForStrength() {
+    this.caculateSum;
+  }
+  get rollForDexterity() {
+    this.caculateSum;
+  }
+  get rollForConstitution() {
+    this.caculateSum;
+  }
+  get rollForIntelligence() {
+    this.caculateSum;
+  }
+  get rollForWisdom() {
+    this.randomDiceRoll;
+  }
+  get rollForCharisma() {
+    this.randomDiceRoll;
+  }
+}*/
